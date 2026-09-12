6883834a9d044f25eb7e711d56852337a0342519
jef <esteronjeferson16@gmail.com>
2026-09-12 23:49:33 +0800

    add: 50%

--- Expense Tracker/script.js
+++ Expense Tracker/script.js
@@ -1,12 +1,14 @@
 
-const addExpense = document.querySelectorAll(".add-expense, .empty-action");
+const addExpenseModal = document.querySelectorAll(".add-expense, .empty-action");
+const addExpenseBtn = document.querySelector(".save-expense");
+const modalForm = document.querySelector(".modal-form");
 const modalBackdrop = document.querySelector(".modal-backdrop");
 const closeModal = document.querySelectorAll(".close-modal, .cancel-button");
 const expenseList = document.querySelector(".expense-list");
 const emptyState = document.querySelector(".empty-state");
 const balance = document.querySelector(".balance");
 const currentMonth = new Date().getMonth() + 1;
-const expenses = [
+let expenses = [
     {id: 1, name: "Fudgee Bar", category: "Food", amount: 16.75, date: "2026-09-09"},
     {id: 2, name: "Fudgee Bar", category: "Food", amount: 16.75, date: "2026-09-09"},
     {id: 3, name: "Fudgee Bar", category: "Food", amount: 16.75, date: "2026-09-09"},
@@ -18,9 +20,7 @@ const expenses = [
 
 function display() {
     balance.textContent = `${expenses.reduce((acc, cur) => {
-        let total = acc + cur.amount;
-
-        return total;
+        return acc + cur.amount;
     }, 0)}`
     if (expenses.length === 0) {
         emptyState.removeAttribute("hidden");
@@ -36,11 +36,25 @@ function display() {
 
 
 
-addExpense.forEach(button => {
+addExpenseModal.forEach(button => {
     button.addEventListener("click", () => {
         modalBackdrop.removeAttribute("hidden");
     });
 });
+// TODO: Make this an eventlistener for the actual form make it submit action
+addExpenseBtn.addEventListener("click", () => {
+    const formData = new FormData(modalForm);
+    const newExpense = {
+        id: date.now(),
+        name: formData.get("name"),
+        category: formData.get("category"),
+        amount: formData.get("amount"),
+        date: formData.get("date")
+    };
+    expenses.push(newExpense);
+    display();
+});
+
 
 closeModal.forEach(button => {
     button.addEventListener("click", () => {
